139916f1685af30edaac17e1baf94567a603d3e1
Brandon Bohorquez <ba.bohorquez10@uniandes.edu.co>
2020-02-03 23:50:23 -0500

    DB. Endpoint 2.

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	"github.com/buaazp/fasthttprouter"
+	_ "github.com/lib/pq"
 	"github.com/valyala/fasthttp"
 )
 
@@ -33,6 +35,15 @@ type ServerResponse struct {
 
 type ServersResponse struct {
 	Servers []ServerResponse `json:"servers"`
+}
+
+type ItemResponse struct {
+	Domain  string           `json:"domain"`
+	Servers []ServerResponse `json:"servers"`
+}
+
+type ItemsResponse struct {
+	Items []ItemResponse `json:"items"`
 }
 
 // Index .
@@ -67,7 +78,7 @@ func ServerInfo(ctx *fasthttp.RequestCtx) {
 		} else if ser.Status == "IN_PROGRESS" {
 			time.Sleep(10000 * time.Millisecond)
 		} else if ser.Status == "ERROR" {
-			// Handle error
+			log.Fatal("Error fetching data")
 		}
 
 	}
@@ -79,6 +90,8 @@ func ServerInfo(ctx *fasthttp.RequestCtx) {
 		// println(endpoint.IPAddress)
 	}
 
+	InsertHostInfo(ser)
+
 	ctx.Response.Header.SetCanonical(strContentType, strApplicationJSON)
 	json.NewEncoder(ctx).Encode(servers)
 
@@ -87,10 +100,139 @@ func ServerInfo(ctx *fasthttp.RequestCtx) {
 	// println(string(bodyBytes))
 }
 
+func InsertEndpointInfo(newEndpoint Endpoint, hostID int) {
+
+	db, err := sql.Open("postgres", "postgresql://root@localhost:26257/domains_db?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	var returnedID int
+	err2 := db.QueryRow(
+		`INSERT INTO endpoints_info (address, ssl_grade, domain_id) 
+		VALUES ($1, $2, $3) RETURNING endpoint_id;`, newEndpoint.IPAddress, newEndpoint.Grade, hostID).Scan(&returnedID)
+
+	if err2 != nil {
+		log.Fatal("Error executing query: ", err2)
+	}
+
+}
+
+func InsertHostInfo(newServer Server) {
+
+	db, err := sql.Open("postgres", "postgresql://root@localhost:26257/domains_db?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("error connecting to the database: ", err)
+	}
+
+	var returnedID int
+	err2 := db.QueryRow(
+		`INSERT INTO domains_info (host_name) 
+		VALUES ($1) RETURNING domain_id;`, newServer.Host).Scan(&returnedID)
+
+	if err2 != nil {
+		log.Fatal("Error executing query: ", err2)
+	}
+
+	for _, endpoint := range newServer.Endpoints {
+		InsertEndpointInfo(endpoint, returnedID)
+	}
+
+}
+
+func GetHostsInfo(ctx *fasthttp.RequestCtx) {
+
+	db, err := sql.Open("postgres", "postgresql://root@localhost:26257/domains_db?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	// Select Statement.
+	rows, err := db.Query("SELECT domain_id, host_name FROM domains_info;")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	items := ItemsResponse{}
+
+	for rows.Next() {
+		var domainID int
+		var hostName string
+
+		if err := rows.Scan(&domainID, &hostName); err != nil {
+			log.Fatal(err)
+		}
+
+		rows2, err := db.Query(`SELECT address, ssl_grade FROM endpoints_info WHERE domain_id = $1;`, domainID)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer rows2.Close()
+
+		item := ItemResponse{Domain: hostName}
+
+		for rows2.Next() {
+			var address string
+			var ssl_grade string
+
+			if err := rows2.Scan(&address, &ssl_grade); err != nil {
+				log.Fatal(err)
+			}
+
+			item.Servers = append(item.Servers, ServerResponse{Address: address, SslGrade: ssl_grade})
+		}
+
+		items.Items = append(items.Items, item)
+
+	}
+
+	ctx.Response.Header.SetCanonical(strContentType, strApplicationJSON)
+	json.NewEncoder(ctx).Encode(items)
+}
+
+func InitDB() {
+	db, err := sql.Open("postgres", "postgresql://root@localhost:26257/domains_db?sslmode=disable")
+
+	if err != nil {
+		log.Fatal("Error connecting to the database: ", err)
+	}
+
+	if _, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS "domains_info" (
+			"domain_id" SERIAL,
+			"host_name" STRING(100),
+			PRIMARY KEY ("domain_id")
+			);`); err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS "endpoints_info" (
+				"endpoint_id" SERIAL,
+				"address" STRING(100),
+				"ssl_grade" STRING(50),
+				"domain_id" INT NOT NULL REFERENCES "domains_info" (domain_id) ON DELETE CASCADE,
+				PRIMARY KEY ("endpoint_id")
+		);`); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
+
+	InitDB()
+
 	router := fasthttprouter.New()
 	router.GET("/", Index)
 	router.GET("/servers/:host", ServerInfo)
+	router.GET("/history", GetHostsInfo)
 
-	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
+	log.Fatal(fasthttp.ListenAndServe(":8081", router.Handler))
 }
